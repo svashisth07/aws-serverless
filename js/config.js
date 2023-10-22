9cf751a86c547e9a5bd18484bfe1e538e1279200
sandy vashisth <sandyvashisth@sandys-MacBook-Pro.local>
2023-10-22 23:39:20 +0530

    new_config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "eu-north-1_UAlvV5bQl", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "6q24cd9i88h9j1g3ggr0ugo98e", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
